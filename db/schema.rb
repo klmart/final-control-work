9a3f641bea057b5bf9916dc021e0a5dea840590a
klmart <kulbaev.mart@gmail.com>
2016-02-20 12:41:48 +0600

    Created model question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220054644) do
+ActiveRecord::Schema.define(version: 20160220064040) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string   "name"
