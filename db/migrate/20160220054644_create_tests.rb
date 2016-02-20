2386d639ee7a09135709f7325ef7fc82d4bb4950
klmart <kulbaev.mart@gmail.com>
2016-02-20 12:08:59 +0600

    Изменена миграция test

--- db/migrate/20160220054644_create_tests.rb
+++ db/migrate/20160220054644_create_tests.rb
@@ -3,6 +3,7 @@ class CreateTests < ActiveRecord::Migration
     create_table :tests do |t|
       t.string :name
       t.string :link
+      t.text :description
 
       t.timestamps null: false
     end
